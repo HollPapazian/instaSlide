7342612195f0cb144d44edba2ed9e4d344af5d3a
Anton Volkov <anton@wolkow.ru>
2025-04-23 21:48:04 +0300

    fix logo

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -2,8 +2,8 @@ export const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4 fixed top-0 w-full">
       <div className="max-w-[1024px] mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-2">
-          <img src="/assets/logo.webp" alt="instaslide logo" className="h-10 w-auto" />
+        <div className="flex items-center gap-3">
+          <img src="/assets/logo_mini.webp" alt="instaslide logo" className="h-10 w-auto" />
           <h1 className="text-2xl font-bold">InstaSlide</h1>
         </div>
       </div>
