a857be36976545a42de031609a05f2a3351f590e
Anton Volkov <anton@wolkow.ru>
2025-04-26 20:43:20 +0300

    position fixed

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="bg-gray-800 text-white py-4 fixed top-0 w-full z-25">
+    <header className="bg-gray-800 text-white py-4 fixed left-0 right-0 top-0 w-full z-50">
       <div className="max-w-[1024px] mx-auto flex justify-between items-center px-4">
         <div className="flex items-center gap-3">
           <img src="/assets/logo_mini.webp" alt="instaslide logo" className="h-10 w-auto" />
