c3638190feaef2ccd84551ca31cda3aa0a6fd364
Anton Volkov <anton@wolkow.ru>
2025-04-28 19:41:01 +0300

    remove sw

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { register } from './serviceWorkerRegistration'
 
 createRoot(document.getElementById('root')!).render(
     <App />,
 )
-
-// Register service worker
-register()
